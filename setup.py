2f41fa343f8583ded8a1958434191ea214b20d41
Scott Merkling <sbmerkling@gmail.com>
2015-09-15 21:10:23 -0700

    Cleaning up messiness

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
         name='badgermole',
         packages=['badgermole'],
-        version='0.1',
+        version='0.2',
         description='A command line argument library',
         author='Scott Merkling',
         author_email='sbmerkling@gmail.com',
         url='https://github.com/Codonaut/badgermole',
-        download_url='https://github.com/Codonaut/badgermole/tarball/0.1',
+        download_url='https://github.com/Codonaut/badgermole/tarball/0.2',
         keywords=['command', 'argument', 'parsing', 'parse'],
         classifiers=[],
 )
